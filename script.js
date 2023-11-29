58c9afe02054ecfdc676366548d694e5e16b5eac
hogepani <152399209+hogepani@users.noreply.github.com>
2023-11-29 22:59:56 +0900

    Add files via upload
    update

--- script.js
+++ script.js
@@ -11,10 +11,18 @@ function generateImage() {
   var textCanvas = document.getElementById("textCanvas");
   var ctx = textCanvas.getContext("2d");
 
+  // ベース画像を非表示にする
+  baseImage.style.display = "none";
+
+  // ベースとなる画像とCanvas要素を取得
+  var baseImage = document.getElementById("baseImage");
+  var textCanvas = document.getElementById("textCanvas");
+  var ctx = textCanvas.getContext("2d");
+
   // Canvasのサイズをベース画像と同じに設定
   textCanvas.width = baseImage.width;
   textCanvas.height = baseImage.height;
-
+  
   // ベース画像をCanvasに描画
   ctx.drawImage(baseImage, 0, 0);
 
@@ -24,12 +32,13 @@ function generateImage() {
 
   ctx.fillText(inputText1, 200, 390); // テキスト1の位置
   ctx.fillText(inputText2, 200, 600); // テキスト2の位置
-  ctx.fillText(inputText3, 650, 390); // テキスト3の位置
-  ctx.fillText(inputText4, 650, 600); // テキスト4の位置
+  ctx.fillText(inputText3, 630, 390); // テキスト3の位置
+  ctx.fillText(inputText4, 630, 600); // テキスト4の位置
 
   // ベース画像を非表示にする
   baseImage.style.display = "none";
 
   // 生成された画像を表示
+  textCanvas.style.maxWidth = "100%"; // 画像の最大幅を親要素に合わせる
   textCanvas.style.display = "block";
 }
