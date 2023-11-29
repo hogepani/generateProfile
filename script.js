de03d15d552878b1205a0af53df7312f1926dc34
hogepani <152399209+hogepani@users.noreply.github.com>
2023-11-30 00:32:39 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -56,23 +56,5 @@ function generateImage() {
   // });
 
   // 画像をクリックしてシェアダイアログを表示
-  textCanvas.addEventListener("touchstart", function(event) {
-    if (navigator.share) {
-      navigator.share({
-        title: "Generated Image",
-        text: "Check out this generated image!",
-        url: textCanvas.toDataURL(),
-      })
-      .then(() => console.log("Shared successfully"))
-      .catch((error) => console.error("Error sharing:", error));
-    } else {
-      // ブラウザがnavigator.shareをサポートしていない場合、ダウンロード用のリンクを作成
-      var downloadLink = document.createElement("a");
-      downloadLink.href = textCanvas.toDataURL();
-      downloadLink.download = "generated_image.jpg";
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-      document.body.removeChild(downloadLink);
-    }
-  });
+
 }
